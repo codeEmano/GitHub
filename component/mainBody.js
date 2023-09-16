3dd597d65d070504452c25c17f0a2507b9bc2248
Prakhar Tiwari <prakhargame@gmail.com>
2023-09-16 23:08:35 +0530

    New content added

--- component/mainBody.js
+++ component/mainBody.js
@@ -5,7 +5,10 @@ function body({heading,links,addedLink}) {
     console.log(`Added one more link...${addedLink}`)
     console.log(`This is an additional statement....`)
 }
+body({heading:"Learning git",links:["link1","link2"],addedLink:"link3"});
+
 function subtract(a,b){
     return a-b;
 }
-body({heading:"Learning git",links:["link1","link2"],addedLink:"link3"});
+
+module.exports =  subtract ;
---
+++ component/new-content.js
@@ -0,0 +1,5 @@
+const subtract=require(`./mainBody`);
+
+const result=subtract(5,2);
+
+console.log(`Result = ${result}`);
