74e4cf71b222cbbcec3e81043c59e60d821e133a
Prakhar Tiwari <prakhargame@gmail.com>
2023-09-16 20:10:26 +0530

    New link added

--- component/mainBody.js
+++ component/mainBody.js
@@ -1,7 +1,8 @@
 console.log("This has the main body");
 
-function body({heading,links}) {
+function body({heading,links,addedLink}) {
     console.log(`The main body is created with the heading ${heading} and links ${links}`)
+    console.log(`Added one more link...${addedLink}`)
 }
 
-body({heading:"Learning git",links:["link1","link2"]});
+body({heading:"Learning git",links:["link1","link2"],addedLink:"link3"});
