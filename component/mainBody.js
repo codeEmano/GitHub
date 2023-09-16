d27f9d5b39641df45946610b062ce6a92a092be2
Prakhar Tiwari <prakhargame@gmail.com>
2023-09-16 23:07:12 +0530

    New content added

--- component/mainBody.js
+++ component/mainBody.js
@@ -5,5 +5,7 @@ function body({heading,links,addedLink}) {
     console.log(`Added one more link...${addedLink}`)
     console.log(`This is an additional statement....`)
 }
-
+function subtract(a,b){
+    return a-b;
+}
 body({heading:"Learning git",links:["link1","link2"],addedLink:"link3"});
